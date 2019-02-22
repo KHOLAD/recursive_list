cd6bba9f13d101c0092087e779036ee654666dc4
Oleg <olegin64@gmail.com>
2019-02-22 02:01:02 +0200

    WIP - recursive list base, Bootstap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,75 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'component-tree';
+
+  public list = [
+    {
+      title: 'childless',
+      children: []
+    },
+    {
+      title: 'great grandparent',
+      children: [
+        {
+          title: 'childless grandsibiling',
+          children: []
+        },
+        {
+          title: 'grandparent',
+          children: [
+            {
+              title: 'childless sibiling',
+              children: []
+            },
+            {
+              title: 'another childless sibiling',
+              children: []
+            },
+            {
+              title: 'parent',
+              children: [
+                {
+                  title: 'child',
+                  children: []
+                },
+                {
+                  title: 'another child',
+                  children: []
+                },
+              ]
+            },
+            {
+              title: 'another parent',
+              children: [
+                {
+                  title: 'child',
+                  children: []
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    },
+  ];
+
+  onAdd(item) {
+    item.push({title: "value", children:[]})
+  }
+
+  onRemoveItem(item) {
+    console.log("remove this:", item)
+  }
+
+  onParent(p) {
+    console.log(p)
+  }
+
+  addCategory(name: string) {
+    this.list.unshift({title: name, children:[]})
+  }
+
 }
